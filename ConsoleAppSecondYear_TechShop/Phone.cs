b8d84087fc401ea5348d0ca342b7e6d12a3592e1
ketig <41895005+ketig@users.noreply.github.com>
2023-12-16 11:48:15 +0400

    Add project files.

---
+++ ConsoleAppSecondYear_TechShop/Phone.cs
@@ -0,0 +1,45 @@
+﻿namespace ConsoleAppSecondYear_TechShop
+{
+    class Phone
+    {
+        private string _brand;
+        private string _price;
+        private string _model;
+        private uint _RAM; // unsigned int - უნიშნო
+        private uint _memory;
+        private uint _quantity;
+
+        public string GetBrand()
+        {
+            return _brand;
+        }
+
+        public string GetPrice()
+        {
+            return _price;
+        }
+
+        public void SetPrice(string value)
+        {
+            _price = value;
+        }
+
+        public uint GetQuantity()
+        {
+            return _quantity;
+        }
+
+        public Phone(
+            string brand, string price,
+            string model, uint RAM,
+            uint memory, uint quantity)
+        {
+            _brand = brand;
+            _price = price;
+            _model = model;
+            _RAM = RAM;
+            _memory = memory;
+            _quantity = quantity;
+        }
+    }
+}
---
+++ ConsoleAppSecondYear_TechShop/Program.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleAppSecondYear_TechShop;
+
+Phone phone = new Phone("Samsung", "400$", "Galaxy",
+    8, 288, 47);
+
+Console.WriteLine($"Quantity of phone is {phone.GetQuantity()}");
+Console.WriteLine($"Brand of phone is {phone.GetBrand()}");
+Console.WriteLine($"Price of phone is {phone.GetPrice()}");
+
+phone.SetPrice("550$");
